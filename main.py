5d5c6eb8ad196846b27c8fc65cbdf20beae0b8b4
Le107 <157118445+Le107@users.noreply.github.com>
2024-03-20 23:40:41 +0300

    Add files via upload
    Автокликер с распознаванием изображений. Ищет совпадения(точность 70%, можно изменить в коде: confidence=0.7) на экране с изображениями в папке Images в алфавитном порядке. Горячие клавиши: Пробел - старт; x - пауза; Esc - стоп.

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import time
+from pathlib import Path
+
+import keyboard
+import pyautogui
+
+os.chdir('Images')
+
+
+def run():
+    for item in Path('.').glob('*'):
+        if item.is_file():
+            try:
+                button = pyautogui.locateOnScreen(str(item), confidence=0.7)
+                pyautogui.click(button)
+                pyautogui.sleep(1)
+            except pyautogui.ImageNotFoundException:
+                pass
+
+
+def main():
+    time.sleep(1)
+    status = 0
+    while True:
+        if keyboard.is_pressed('space'):
+            status = 1
+        elif keyboard.is_pressed('x'):
+            status = 0
+        elif keyboard.is_pressed('esc'):
+            break
+        elif status == 1:
+            run()
+        else:
+            pass
+
+
+main()
